8db52e41efd424be820744f4849b93c5948fc7df
urgmaker <uranovrg@gmail.com>
2023-09-13 15:04:29 +0300

    Prepare ExchangeRatesServlet to logic implementation

---
+++ src/main/java/com/example/currencyexchange/servlets/exchanges/ExchangeRatesServlet.java
@@ -0,0 +1,9 @@
+package com.example.currencyexchange.servlets.exchanges;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "ExchangeRatesServlet", urlPatterns = "/exchangeRates")
+public class ExchangeRatesServlet extends HttpServlet {
+    // TODO: implements GET - request logic
+}
